24cd71ac1b5c9ca09f08a13d8a0d35f752909527
Guide42-ho <mophone563@gmail.com>
2025-08-19 13:49:27 +0700

    TicketStatus
    make TicketStatus

---
+++ src/pages/TicketStatus.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TicketStatus: React.FC = () => {
+    return (
+        <div>
+            <h1>Ticket Status Page</h1>
+            <p>This page will display the status of your tickets.</p>
+        </div>
+    );
+}
+
+export default TicketStatus;
--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Reserve from "./pages/Reserve";
+import TicketStatus from "./pages/TicketStatus";
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/Home" element={<Home />} />
             <Route path="/Reserve" element={<Reserve />} />
+            <Route path="/TicketStatus" element={<TicketStatus />} />
         </Routes>
     );
 }
