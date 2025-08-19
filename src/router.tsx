11a581975e9945ff4b1bbe84dd7104f501e41f20
Thanachot12345 <perthza12e34@gmail.com>
2025-08-19 15:13:34 +0700

    delete cancle ticket
     delete

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Reserve from "./pages/Reserve";
 import TicketStatus from "./pages/TicketStatus";
-import CancelTicket from "./pages/CancelTicket";
 import ConfirmAttendance from "./pages/ConfirmAttendance";
 import SearchTickets from "./pages/SearchTickets";
 
@@ -13,7 +12,6 @@ export default function AppRoutes() {
             <Route path="/Home" element={<Home />} />
             <Route path="/Reserve" element={<Reserve />} />
             <Route path="/TicketStatus" element={<TicketStatus />} />
-            <Route path="/CancelTicket" element={<CancelTicket />} />
             <Route path="/ConfirmAttendance" element={<ConfirmAttendance />} />
             <Route path="/SearchTickets" element={<SearchTickets />} />
         </Routes>
