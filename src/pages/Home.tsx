77d2d4423488ac4f643fd919babf522e56dcaf2d
Guide42-ho <mophone563@gmail.com>
2025-08-22 13:46:41 +0700

    edit home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
       try {
         const data = await searchTickets({all: true});
         if (data.status === "Success") {
-          setQueue(data.ticket.lenght);
+          setQueue(data.ticket.length);
         } else {
           console.error("Error fetching tickets:", data.message);
         }
@@ -43,7 +43,7 @@ const Home: React.FC = () => {
           {/* กล่องคิวที่รอ */}
           <div className="rounded-xl border border-gray-300 bg-white shadow-sm p-6 text-center flex flex-col justify-center">
             <div className="text-4xl mb-2">👥</div>
-            <h1 className="text-3xl font-bold">12</h1>
+            <h1 className="text-3xl font-bold">{queue}</h1>
             <p className="text-gray-500">คิวที่รอ</p>
           </div>
 
