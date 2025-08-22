86b26f9f4ce8c89c6fd243acdd57349336b99600
Guide42-ho <mophone563@gmail.com>
2025-08-22 12:56:51 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { config } from './src/config/config'
+// import { config } from './src/config/config'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(),],
