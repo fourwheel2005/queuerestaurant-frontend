d437dbe3eff1abda83e4b9097392e6976a37e163
Thanachot12345 <perthza12e34@gmail.com>
2025-08-22 13:49:47 +0700

    update

--- src/pages/Reserve.tsx
+++ src/pages/Reserve.tsx
@@ -70,13 +70,11 @@ const Reserve: React.FC = () => {
                     />
 
                     <input
-                        type="tel"
+                        type="number"
                         name="phone"
                         placeholder="เบอร์โทรศัพท์"
                         value={formTicket.phone}
                         onChange={handleChange}
-                        maxLength={10}
-                        pattern="[0-9]*"
                         className="w-full mb-4 border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
                     />
 
